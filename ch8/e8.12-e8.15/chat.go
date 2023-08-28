1dec79d0ac5f391fd5652d3a85f5cb3b5c8ce65a
Pieere Pi <pihuibin@hotmail.com>
2023-08-28 22:06:36 +0800

    完善8.12-8.15聊天服务器程序

--- ch8/e8.12-e8.15/chat.go
+++ ch8/e8.12-e8.15/chat.go
@@ -12,56 +12,118 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"strings"
+	"time"
 )
 
-//!+broadcaster
+// !+broadcaster
+const (
+	ENTERING  = 0
+	LEAVING   = 1
+	MESSAGING = 2
+	TIMEOUT   = 3
+)
+
+const IDLETIMEOUT = 5 * time.Minute
+
+type clientChannel chan<- string
+
+type msg struct {
+	msgtype int           // ENTERING, LEAVING, MESSAGING, TIMEOUT
+	channel clientChannel // client outgoing message channel
+	message string        // ENTERING - addr, MESSAGING - message, other - empty
+	sender  string        // ENTERING - sender name, other - empty
+}
+
 type client struct {
-	channel chan<- string // an outgoing message channel
-	addr    string
+	channel  clientChannel // an outgoing message channel
+	addr     string
+	name     string
+	lasttime time.Time
 }
 
 var (
-	entering = make(chan client)
-	leaving  = make(chan client)
-	messages = make(chan string) // all incoming client messages
+	clients  = make(map[clientChannel]client) // all connected clients
+	messages = make(chan msg)                 // all incoming client messages
 )
 
+func writeOrSkip(channel clientChannel, message string) {
+	select {
+	case channel <- message:
+	default:
+	}
+}
+
 func broadcaster() {
-	clients := make(map[client]bool) // all connected clients
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
 	for {
 		select {
 		case msg := <-messages:
-			// Broadcast incoming message to all
-			// clients' outgoing message channels.
-			wi := strings.Index(msg, ": ")
-			for cli := range clients {
-				addr := cli.addr
-				//cli.channel <- msg
-				if !(wi != -1 && addr == msg[:wi]) {
-					cli.channel <- msg
+			switch msg.msgtype {
+			case ENTERING:
+				message := "\t\t" + msg.sender + " 进入了聊天室。"
+				for channel := range clients {
+					writeOrSkip(channel, message)
+				}
+				clients[msg.channel] = client{msg.channel, msg.message, msg.sender, time.Now()}
+			case LEAVING, TIMEOUT:
+				var sender string
+				var message string
+				if _, ok := clients[msg.channel]; !ok {
+					continue
+				} else {
+					sender = clients[msg.channel].name
+					if msg.msgtype == TIMEOUT {
+						writeOrSkip(msg.channel, sender+" 由于你长时间没有反应，你已被移出了聊天室。")
+					}
+					// let clientWriter exit the loop and then close the connection
+					close(msg.channel)
+					delete(clients, msg.channel)
+				}
+				if msg.msgtype == LEAVING {
+					message = "\t\t" + sender + " 离开了聊天室。"
+				} else {
+					message = "\t\t" + sender + " 长时间没有反应，已经离开了聊天室。"
+				}
+				for channel := range clients {
+					writeOrSkip(channel, message)
+				}
+			case MESSAGING:
+				// Broadcast incoming message to all
+				// clients' outgoing message channels.
+				var sender string
+				if _, ok := clients[msg.channel]; !ok {
+					continue
+				} else {
+					sender = clients[msg.channel].name
+					// update lasttime
+					clients[msg.channel] = client{clients[msg.channel].channel, clients[msg.channel].addr, clients[msg.channel].name, time.Now()}
+				}
+				message := "\t\t" + sender + " 说: " + msg.message
+				for channel := range clients {
+					if channel != msg.channel {
+						writeOrSkip(channel, message)
+					}
 				}
 			}
+		case <-ticker.C:
+			for _, cli := range clients {
+				if cli.lasttime.Add(IDLETIMEOUT).Before(time.Now()) {
+					lch := cli.channel
+					go func() { messages <- msg{TIMEOUT, lch, "", ""} }()
 
-		case cli := <-entering:
-			clients[cli] = true
-
-		case cli := <-leaving:
-			delete(clients, cli)
-			close(cli.channel)
+				}
+			}
 		}
 	}
 }
 
-//!-broadcaster
-
-//!+handleConn
 func handleConn(conn net.Conn) {
-	ch := make(chan string) // outgoing client messages
 	var who string
+	ch := make(chan string, 5) // outgoing client messages, buffered channel
 	go clientWriter(conn, ch)
 
-	fmt.Fprintln(conn, "请输入你的名称:")
+	fmt.Fprint(conn, "请输入你的名称：")
 	input := bufio.NewScanner(conn)
 	if input.Scan() {
 		who = input.Text()
@@ -69,19 +131,15 @@ func handleConn(conn net.Conn) {
 		conn.Close()
 		return
 	}
-	//who := conn.RemoteAddr().String()
-	ch <- "You are " + who
-	messages <- who + " has arrived"
-	entering <- client{ch, who}
+	ch <- who + " 你好！"
+	messages <- msg{ENTERING, ch, conn.RemoteAddr().String(), who}
 
-	//input := bufio.NewScanner(conn)
 	for input.Scan() {
-		messages <- who + ": " + input.Text()
+		messages <- msg{MESSAGING, ch, input.Text(), ""}
 	}
 	// NOTE: ignoring potential errors from input.Err()
 
-	leaving <- client{ch, who}
-	messages <- who + " has left"
+	messages <- msg{LEAVING, ch, "", ""}
 	conn.Close()
 }
 
@@ -89,11 +147,10 @@ func clientWriter(conn net.Conn, ch <-chan string) {
 	for msg := range ch {
 		fmt.Fprintln(conn, msg) // NOTE: ignoring network errors
 	}
+	//fmt.Println("close conn")
+	conn.Close()
 }
 
-//!-handleConn
-
-//!+main
 func main() {
 	listener, err := net.Listen("tcp", "localhost:8000")
 	if err != nil {
@@ -110,5 +167,3 @@ func main() {
 		go handleConn(conn)
 	}
 }
-
-//!-main
